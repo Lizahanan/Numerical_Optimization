3d5c52752e9906dcc725b02c66aa76db17dc5732
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2025-05-23 17:22:05 +0300

    utils implemented

--- HW1/src/utils.py
+++ HW1/src/utils.py
@@ -10,10 +10,57 @@ def plot_contours_with_paths(f, xlim, ylim, paths=None, labels=None, title="Cont
     - paths: list of lists of x points
     - labels: method names for legend
     """
-    pass
+    # Create a grid of points
+    x1 = np.linspace(xlim[0], xlim[1], 400)
+    x2 = np.linspace(ylim[0], ylim[1], 400)
+    X1, X2 = np.meshgrid(x1, x2)
+
+    # Compute the function values on the grid
+    Z = np.zeros_like(X1)
+    for i in range(X1.shape[0]):
+        for j in range(X1.shape[1]):
+            Z[i, j],_,_ = f(np.array([X1[i, j], X2[i, j]]), need_hessian=False)
+
+    # Plot the contours
+    plt.figure(figsize=(8, 6))
+    contour = plt.contour(X1, X2, Z, levels=levels, cmap='viridis')
+    plt.clabel(contour, inline=True, fontsize=8)
+
+    #optionally print paths 
+    colors = ['r', 'b', 'g', 'm', 'c', 'y', 'orange', 'purple']
+    markers = ['o', 's', 'D', '^', 'v', '*']
+    if paths is not None:
+        for i, path in enumerate(paths):
+            path = np.array(path)
+            color = colors[i % len(colors)]
+            marker = markers[i % len(markers)]
+            plt.plot(path[:, 0], path[:, 1], color=color, marker=marker, label=labels[i] if labels else f'Method {i+1}')
+    
+    # plot
+    plt.title(title)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    if paths:
+        plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+    
 
 def plot_function_values(f_paths, labels, title="Function Value per Iteration"):
     """
     Plots f(x) vs iteration number for each method with colored lines.
     """
-    pass
+    #plot figure 
+    plt.figure(figsize=(8, 6))
+    colors = ['r', 'b', 'g', 'm', 'c', 'y']
+    for i, f_vals in enumerate(f_paths):
+        color = colors[i % len(colors)]
+        plt.plot(f_vals, color=color, label=labels[i])
+    plt.title(title)
+    plt.xlabel('Iteration')
+    plt.ylabel('Function Value')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
