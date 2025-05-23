dd5abc4a88daf9c45eacc63c8ad346176962f3a1
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2025-05-23 15:56:10 +0300

    Removed code duplication for cleaner code

--- HW1/tests/examples.py
+++ HW1/tests/examples.py
@@ -6,26 +6,14 @@ def quad_circle(x, need_hessian=False):
     Quadratic function with identity Q matrix (circle contours).
     """
     Q = np.eye(2) # Identity matrix for circle -> 2 x 2
-    #the function is of the form x^T Q x
-    f = x.T @ Q @ x 
-    #gradient is of the form 2 Q x
-    grad = 2 * Q @ x
-    #hessian is constant and equal to 2 Q
-    hess = 2 * Q if need_hessian else None
-    return f, grad, hess
+    return compute_Quads(x, Q, need_hessian)
 
 def quad_ellipse(x, need_hessian=False):
     """
     Quadratic function with axis-aligned ellipse Q matrix.
     """
     Q = np.array([[1, 0], [0, 100]]) # Ellipse matrix
-    #the function is of the form x^T Q x
-    f = x.T @ Q @ x
-    #gradient is of the form 2 Q x
-    grad = 2 * Q @ x
-    #hessian is constant and equal to 2 Q
-    hess = 2 * Q if need_hessian else None
-    return f, grad, hess
+    return compute_Quads(x, Q, need_hessian)
 
 def quad_rotated_ellipse(x, need_hessian=False):
     """
@@ -33,6 +21,10 @@ def quad_rotated_ellipse(x, need_hessian=False):
     """
     R = np.array([[sqrt(3)/2, -0.5], [0.5, sqrt(3)/2]]) # Rotation matrix
     Q = R.T @ np.array([[100, 0], [0, 1]]) @ R # Ellipse matrix
+    return compute_Quads(x, Q, need_hessian)
+
+def compute_Quads(x, Q, need_hessian):
+    '''Computes the quadratic function, gradient and hessian(if needed) for a given Q matrix'''
     #the function is of the form x^T Q x
     f = x.T @ Q @ x
     #gradient is of the form 2 Q x
