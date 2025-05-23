54026cad1dd407225f6dfa7caf47c7e7b0644dd4
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2025-05-23 14:57:34 +0300

    implemented the termination criteria

--- HW1/src/unconstrained_min.py
+++ HW1/src/unconstrained_min.py
@@ -17,15 +17,24 @@ def _gradient_descent_step(grad):
 def _newton_step(grad, hess):
     """
     Returns the Newton direction: -H⁻¹ * grad
+    we should not however invert the Hessian directly
+    instead we call the linear solver
+    hess * pk = -grad
     """
-    pass
+    return np.linalg.solve(hess, -grad)
 
 
 def _should_stop(prev_f, curr_f, prev_x, curr_x, obj_tol, param_tol):
     """
     Checks if termination criteria are met.
+    we check for the numeric tolerance:
+    1. objective tolerance -> |f(x_k) - f(x_{k-1})| < obj_tol
+    2. parameter tolerance -> ||x_k - x_{k-1}|| < param_tol 
     """
-    pass
+    obj_change = abs(curr_f - prev_f)
+    param_change = np.linalg.norm(curr_x - prev_x)
+    # Check if the change in objective value is less than the tolerance
+    return obj_change < obj_tol or param_change < param_tol
 
 
 def _print_iteration_info(i, x, f_val):
