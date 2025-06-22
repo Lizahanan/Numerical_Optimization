55421b19cb3843fe953bb713981a2a39e49e4942
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2025-06-22 22:41:32 +0300

    started implemetning feasable 2d plot

--- src/utils.py
+++ src/utils.py
@@ -75,3 +75,37 @@ def plot_function_values(f_paths, labels, title="Function Value per Iteration", 
         plt.close()
     else:
         plt.show()
+
+def plot_2d_feasible_region_with_path(ineq_constraints, xlim, ylim, path, title="2D Feasible Region and Central Path", save_path=None):
+    """
+    Plots the 2D feasible region defined by inequality constraints and the optimization path.
+    
+    Parameters:
+    - ineq_constraints: list of constraint functions
+    - xlim, ylim: plotting limits
+    - path: optimization path (list of points)
+    - title: plot title
+    - save_path: path to save the plot
+    """
+    fig, ax = plt.subplots(figsize=(10, 8))
+    
+    # Create a grid for constraint visualization
+    x1 = np.linspace(xlim[0], xlim[1], 400)
+    x2 = np.linspace(ylim[0], ylim[1], 400)
+    X1, X2 = np.meshgrid(x1, x2)
+
+    #find feasable region
+    feasible_region = np.ones_like(X1, dtype=bool)
+
+    for constraint in ineq_constraints:
+        constraint_vals = np.zeros_like(X1)
+        for i in range(X1.shape[0]):
+            for j in range(X1.shape[1]):
+                constraint_vals[i, j], _, _ = constraint(np.array([X1[i, j], X2[i, j]]), need_hessian=False)
+        
+        # Constraint satisfied where g(x) < 0
+        feasible &= (constraint_vals < 0)
+
+    # Plot the feasible region in green 
+    ax.contourf(X1, X2, feasible.astype(int), levels=[0.5, 1.5], colors=['lightgreen'], alpha=0.3, label='Feasible Region')
+    
